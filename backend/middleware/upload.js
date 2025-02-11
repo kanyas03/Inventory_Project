f5038f503e279fd88595fca95f6af64240967271
kanyas03 <kanyas0395@gmail.com>
2025-02-11 09:55:21 +0530

    Update

--- backend/middleware/upload.js
+++
@@ -1,8 +0,0 @@
-import multer from "multer";
-
-// Use memory storage to store file as buffer
-const storage = multer.memoryStorage();
-
-const upload = multer({ storage: storage });
-
-export default upload;
